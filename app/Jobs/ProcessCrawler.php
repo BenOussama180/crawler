2d9a504116e5d70c76b1dfbb1e599fe594fe0ee1
Oussama bendriss <oussama.bendriss@outlook.com>
2025-02-15 19:07:50 +0000

    dashboard

--- app/Jobs/ProcessCrawler.php
+++ app/Jobs/ProcessCrawler.php
@@ -24,8 +24,13 @@ class ProcessCrawler implements ShouldQueue
      */
     public function handle(CrawlerService $crawlerService): void
     {
+        info('Crawling sources for config: ' . $this->crawlerConfig->id);
+
         $sources = $crawlerService->findSourcesWithGoogle($this->crawlerConfig->keywords);
 
+        info('Found ' . count($sources) . ' sources for config: ' . $this->crawlerConfig->id);
+
+        return;
         $crawlerService->crawlForKeywords(
             $this->crawlerConfig->keywords,
             $sources
