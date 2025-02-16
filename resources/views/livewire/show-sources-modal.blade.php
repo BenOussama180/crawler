ba3eb7f7d7bbcdb524785f08a4512bc94a873871
Oussama bendriss <oussama.bendriss@outlook.com>
2025-02-16 18:27:04 +0000

    finish

--- resources/views/livewire/show-sources-modal.blade.php
+++ resources/views/livewire/show-sources-modal.blade.php
@@ -8,6 +8,16 @@
             <small class="text-gray-500">Type: {{ $source['type'] }}</small>
             <p class="mt-1 text-gray-500">URL: <a href="{{ $source['url'] }}" target="_blank"
                     class="text-blue-500 hover:underline">{{ $source['url'] }}</a></p>
+            <div class="mt-2">
+                <span
+                    class="inline-block px-2 py-1 text-xs font-semibold text-white bg-{{ $source['status'] == 'crawled' ? 'green' : 'red' }}-500 rounded-full">
+                    {{ $source['status'] }}
+                </span>
+                @if($source['status'] == 'crawled')
+                <a href="{{ asset('storage/articles/'. parse_url($source['url'], PHP_URL_HOST))}}" target="_blank"
+                    class="ml-2 text-blue-500 hover:underline">Download File</a>
+                @endif
+            </div>
         </li>
         @endforeach
     </ul>
